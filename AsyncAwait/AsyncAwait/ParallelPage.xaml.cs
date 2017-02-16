97652f74a905fd0f47a6de2df388ee282d2803ff
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-02-15 20:45:08 -0600

    Advanced page

--- AsyncAwait/AsyncAwait/ParallelPage.xaml.cs
+++ AsyncAwait/AsyncAwait/ParallelPage.xaml.cs
@@ -8,63 +8,84 @@ using Xamarin.Forms;
 
 namespace AsyncAwait
 {
-    public partial class ParallelPage : ContentPage
-    {
-        public ParallelPage()
-        {
-            InitializeComponent();
-        }
+	public partial class ParallelPage : ContentPage
+	{
+		public ParallelPage()
+		{
+			InitializeComponent();
+		}
 
+		private async void ParallelOneClicked(object sender, EventArgs e)
+		{
+			TaskStatusLabel.Text = "Tarea iniciada";
+			ProgressBar1.Progress = 0;
+			ProgressBar2.Progress = 0;
+			var t1 = EjecutaTareaAsincrona1();
+			var t2 = EjecutaTareaAsincrona2();
+			await Task.WhenAny(t1, t2);
+			TaskStatusLabel.Text = "Tarea terminada";
+		}
 
+		private void BlockingParallelClicked(object sender, EventArgs e)
+		{
+			TaskStatusLabel.Text = "Tarea iniciada";
+			ProgressBar1.Progress = 0;
+			ProgressBar2.Progress = 0;
+			var t1 = EjecutaTareaAsincrona1();
+			var t2 = EjecutaTareaAsincrona2();
+			Task.WaitAll(t1, t2);
+			TaskStatusLabel.Text = "Tarea terminada";
+		}
 
-        private async void ButtonClicked(object sender, EventArgs e)
-        {
-            TaskStatusLabel.Text = "Tarea iniciada";
-            ProgressBar1.Progress = 0;
-            ProgressBar2.Progress = 0;
-            if (sender == ParallelButton)
-            {
-                var t1 = EjecutaTareaAsincrona1();
-                var t2 = EjecutaTareaAsincrona2();
+		private async void ParallelClicked(object sender, EventArgs e)
+		{
+			TaskStatusLabel.Text = "Tarea iniciada";
+			ProgressBar1.Progress = 0;
+			ProgressBar2.Progress = 0;
+			var t1 = EjecutaTareaAsincrona1();
+			var t2 = EjecutaTareaAsincrona2();
+			await Task.WhenAll(t1, t2);
+			TaskStatusLabel.Text = "Tarea terminada";
+		}
 
-                await Task.WhenAll(t1, t2);
-            }
-            else if (sender == SequentialButton)
-            {
-                 await EjecutaTareaAsincrona1();
-                 await EjecutaTareaAsincrona2();
-            }
-            TaskStatusLabel.Text = "Tarea terminada";
-        }
+		private async void SequentialClicked(object sender, EventArgs e)
+		{
+			ProgressBar1.Progress = 0;
+			ProgressBar2.Progress = 0;
+			TaskStatusLabel.Text = "Tarea iniciada";
+			await EjecutaTareaAsincrona1();
+			await EjecutaTareaAsincrona2();
+			TaskStatusLabel.Text = "Tarea terminada";
+		}
 
-        async Task EjecutaTareaAsincrona1()
-        {
-            await Task.Factory.StartNew(() =>
-            {
-                for (int i = 0; i < 100; i++)
-                {
-                    Task.Delay(100).Wait();
-                    Device.BeginInvokeOnMainThread(() =>
-                    {
-                        ProgressBar1.Progress = ((double)i + 1) / 100;
-                    });
-                }
-            });
-        }
+		async Task EjecutaTareaAsincrona1()
+		{
+			await Task.Factory.StartNew(() =>
+			{
+				for (int i = 0; i < 100; i++)
+				{
+					Task.Delay(40).Wait();
+					Device.BeginInvokeOnMainThread(() =>
+					{
+						ProgressBar1.Progress = ((double)i + 1) / 100;
+					});
+				}
+			});
+		}
 
-        async Task EjecutaTareaAsincrona2()
-        {
-            await Task.Factory.StartNew(() =>
-            {
-                for (int i = 0; i < 100; i++)
-                {
-                    Task.Delay(100).Wait();
-                    Device.BeginInvokeOnMainThread(() =>
-                    {
-                        ProgressBar2.Progress = ((double)i + 1) / 100;
-                    });
-                }
-            });
-        }
-    }
+		async Task EjecutaTareaAsincrona2()
+		{
+			await Task.Factory.StartNew(() =>
+			{
+				for (int i = 0; i < 100; i++)
+				{
+					Task.Delay(100).Wait();
+					Device.BeginInvokeOnMainThread(() =>
+					{
+						ProgressBar2.Progress = ((double)i + 1) / 100;
+					});
+				}
+			});
+		}
+	}
 }
